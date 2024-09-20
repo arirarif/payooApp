d76de672d1683718d5259fa9510d8c42788d2cc1
arirarif <arirarif876@gmail.com>
2024-09-20 12:32:27 +0600

    home

--- js/login.js
+++ js/login.js
@@ -9,5 +9,15 @@ document.getElementById('button-login')
         // get phone number
         const phoneNumber = document.getElementById('phone-number').value
         console.log(phoneNumber);
-        
+        const pinNumber = document.getElementById('pin-number').value
+        console.log(pinNumber)
+
+        // 
+        if(phoneNumber === '5' && pinNumber === '1234'){
+            console.log('you are logged in');
+            // step 5 : allow user to use website
+            window.location.href = '/home.html'
+        }else{
+            console.log('Wrong phone or pin number');            
+        }
     })
